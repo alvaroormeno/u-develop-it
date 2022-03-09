9d4782d91ca1efebe7da7a915f9c542150b16f24
alvaroormeno <alvaro@ormeno.org>
2022-03-09 13:21:09 -0500

    connection to database on sever.js DONE

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
+// Import MYSQL2 package which was installed using node
+const mysql = require('mysql2');
+
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Express middleware
+// Express.js middleware
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
+
+// Code that connects application to the MYSQL DATA BASE
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // Your MySQL username,
+      user: 'root',
+      // Your MySQL password
+      password: 'mysqlroot1',
+      database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
 
 
 
