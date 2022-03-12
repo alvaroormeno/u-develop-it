dcf79b9aedbc802165c35fe6fef7c503671ee4c1
alvaroormeno <alvaro@ormeno.org>
2022-03-12 01:37:43 -0500

    all vote route working, added route to count votes

--- routes/apiRoutes/voterRoutes.js
+++ routes/apiRoutes/voterRoutes.js
@@ -37,7 +37,7 @@ router.get('/voter/:id', (req, res) => {
     });
 });
 
-
+// - POST Route to add a voter
 router.post('/voter', ({ body }, res) => {
     const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
     const params = [body.first_name, body.last_name, body.email];
