5469faf82cac9cf0822290a4beaef573a13ec02a
alvaroormeno <alvaro@ormeno.org>
2022-03-11 23:24:49 -0500

    my sql databae seeded complete

--- db/schema.sql
+++ db/schema.sql
@@ -1,5 +1,6 @@
 DROP TABLE IF EXISTS candidates;
 DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
 
 -- CREATE A TABLE FOR PARTIES DATA 
 CREATE TABLE parties (
@@ -16,4 +17,15 @@ CREATE TABLE candidates (
   party_id INTEGER,
   industry_connected BOOLEAN NOT NULL,
   CONSTRAINT fk_party FOREIGN KEY (party_id) REFERENCES parties(id) ON DELETE SET NULL
+);
+
+
+-- CREATE A TABLE FOR VOTERS DATA 
+CREATE TABLE voters (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  email VARCHAR(50) NOT NULL,
+  -- This datetime default statement captures the data and time when voters registered
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP
 );
