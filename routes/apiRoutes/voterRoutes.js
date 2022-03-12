4c0d1411ea03ae1912c10ace66be0222f9b79783
alvaroormeno <alvaro@ormeno.org>
2022-03-12 00:35:13 -0500

    added voter routers

---
+++ routes/apiRoutes/voterRoutes.js
@@ -0,0 +1,116 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+const inputCheck = require('../../utils/inputCheck');
+
+// - This route will perform a SELECT * FROM voters ORDER BY last_name and return
+//   the voter rows in order of Last Name on success or a 500 status if there were errors.
+router.get('/voters', (req, res) => {
+    const sql = `SELECT * FROM voters ORDER BY last_name`;
+  
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows,
+      });
+    });
+});
+
+// Get single voter
+router.get('/voter/:id', (req, res) => {
+    const sql = `SELECT * FROM voters WHERE id = ?`;
+    const params = [req.params.id];
+  
+    db.query(sql, params, (err, row) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: row
+      });
+    });
+});
+
+
+router.post('/voter', ({ body }, res) => {
+    const sql = `INSERT INTO voters (first_name, last_name, email) VALUES (?,?,?)`;
+    const params = [body.first_name, body.last_name, body.email];
+
+    // Data validation
+    const errors = inputCheck(body, 'first_name', 'last_name', 'email');
+    if (errors) {
+        res.status(400).json({ error: errors });
+        return;
+    }
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+});
+
+// PUT Route to change email of voter
+router.put('/voter/:id', (req, res) => {
+    // Data validation
+    const errors = inputCheck(req.body, 'email');
+    if (errors) {
+      res.status(400).json({ error: errors });
+      return;
+    }
+  
+    const sql = `UPDATE voters SET email = ? WHERE id = ?`;
+    const params = [req.body.email, req.params.id];
+  
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Voter not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        });
+      }
+    });
+});
+
+// - This Delete route query doesnt have a param const variable like others, 
+//   instead the param we want is in the query statement itself.
+router.delete('/voter/:id', (req, res) => {
+    const sql = `DELETE FROM voters WHERE id = ?`;
+  
+    db.query(sql, req.params.id, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: res.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Voter not found'
+        });
+      } else {
+        res.json({
+          message: 'deleted',
+          changes: result.affectedRows,
+          id: req.params.id
+        });
+      }
+    });
+});
+
+
+module.exports = router;
